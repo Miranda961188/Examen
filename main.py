26144953e304c065d14afa0c37a860dedb175670
Miranda <ajeletsahar1995@gmail.com>
2023-01-14 21:49:22 -0600

    Se agrega el menu principal

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def menu():
+    print(
+        "Dime, ¿qué operación quieres hacer? \n 1) Suma \n 2) Resta \n 3) Multiplicar \n 4) Dividir \n 5) Exponente \n 6) Raiz cuadrada \n 7) Seno  \n 8) Coseno \n 9) Tangente \n10) Apagar calculadora \n")
+while True:
+    menu()
+    opcion = int(input("Ingrese una opción: "))
+
