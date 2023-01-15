9991bbdc11d6c2743b66b356d5ef9fb875c5cbaa
Miranda <ajeletsahar1995@gmail.com>
2023-01-14 22:06:03 -0600

    Se agrega funcion de operaciones de un componente

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import math
+import numpy as np
 def menu():
     print(
         "Dime, ¿qué operación quieres hacer? \n 1) Suma \n 2) Resta \n 3) Multiplicar \n 4) Dividir \n 5) Exponente \n 6) Raiz cuadrada \n 7) Seno  \n 8) Coseno \n 9) Tangente \n10) Apagar calculadora \n")
@@ -23,6 +24,21 @@ def operaciones(numero1, numero2):
         print("RESULTADO: Base", numero1, "exponente", numero2, "es igual a", numero1 ** numero2, "\n")
     else:
         print("Opcion incorrecta")
+def operacion(numero1):
+    if opcion == 6:
+        print(" ")
+        print("RESULTADO: La raiz cuadrada de", numero1, "es igual a", math.sqrt(numero1), "\n")
+    elif opcion == 7:
+        print(" ")
+        print("RESULTADO: la operacion Seno de", numero1, "es igual a", np.sin(numero1), "\n")
+    elif opcion == 8:
+        print(" ")
+        print("RESULTADO: la operacion Coseo de", numero1, "es igual a", np.cos(numero1), "\n")
+    elif opcion == 9:
+        print(" ")
+        print("RESULTADO: la operacion Tangente de", numero1, "es igual a", np.tan(numero1), "\n")
+    else:
+        print("Opción incorrecta")
 
 while True:
     menu()
@@ -30,6 +46,14 @@ while True:
 
     if opcion == 10:
         break
+    elif opcion == 5:
+        num1 = float(input("Introduce el numero como base:"))
+        num2 = float(input("Introduce el numero para exponente:"))
+        operaciones(num1, num2)
+
+    elif opcion == 6 or opcion == 7 or opcion == 8 or opcion == 9:
+        num1 = float(input("Introduce el número: "))
+        operacion(num1)
 
     else:
         num1 = float(input("Introduce tu primer número:"))
